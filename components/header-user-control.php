318857bfa079b63ced79e2f791911e2a1e99a0c0
PeoScouser98 <quanghiep03198@gmail.com>
2022-06-17 02:34:14 +0700

    Update header-user-control.php

--- components/header-user-control.php
+++ components/header-user-control.php
@@ -15,7 +15,7 @@
         </li>
         <li class="nav-item">
             <div class="position-relative" style="width:3rem">
-                <a class="nav-link" href="index.php?page=cart-list#cart"><i class="fas fa-shopping-cart"></i></a>
+                <a class="nav-link" href="?page=cart-list#cart"><i class="fas fa-shopping-cart"></i></a>
                 <span class="position-absolute top-0 bg-danger text-white rounded-circle text-center align-middle translate-y-50" style="width:1.5rem; height:1.5rem; right:0"><?= isset($_SESSION['cart']) ? count($_SESSION['cart']) : 0 ?></span>
             </div>
         </li>
