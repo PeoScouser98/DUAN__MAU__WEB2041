b058b9722720f963cbb3915f105435a495d4e6f5
PeoScouser98 <quanghiep03198@gmail.com>
2022-06-16 15:38:29 +0700

    update danh sách đơn hàng theo đơn mới nhất

--- admin/order-list.php
+++ admin/order-list.php
@@ -1,9 +1,9 @@
 <?php
-$pagination = paginate_page("SELECT COUNT(`order_id`) FROM `orders`", 10);
-extract($pagination);
 $orderStatus = select_all_records("SELECT * FROM order_status");
 // lọc theo trạng thái
 if (isset($_GET['filterby']) && is_numeric($_GET['filterby'])) {
+    $pagination = paginate_page("SELECT COUNT(`order_id`) FROM `orders` WHERE orders.status_id = {$_GET['filterby']}", 10);
+    extract($pagination);
     $orders = select_all_records("SELECT orders.order_id,
                                         product.product_id,
                                         product.product_name,
@@ -17,9 +17,13 @@ if (isset($_GET['filterby']) && is_numeric($_GET['filterby'])) {
                                         INNER JOIN product ON product.product_id = order_detail.product_id 
                                         WHERE orders.status_id = {$_GET['filterby']}
                                         GROUP BY orders.placed_on
+                                        ORDER BY MONTH(placed_on) DESC, DAY(placed_on) DESC
                                         LIMIT {$startIndex}, {$qty}");
-    // lấy tất cả
-} else
+}
+// lấy tất cả
+else {
+    $pagination = paginate_page("SELECT COUNT(`order_id`) FROM `orders`", 10);
+    extract($pagination);
     $orders = select_all_records("SELECT orders.order_id,
                                         product.product_id,
                                         product.product_name,
@@ -33,7 +37,29 @@ if (isset($_GET['filterby']) && is_numeric($_GET['filterby'])) {
                                         INNER JOIN order_status ON orders.status_id = order_status.status_id 
                                         INNER JOIN product ON product.product_id = order_detail.product_id 
                                         GROUP BY orders.placed_on
+                                        ORDER BY MONTH(placed_on) DESC, DAY(placed_on) DESC
                                         LIMIT {$startIndex}, {$qty}");
+}
+if (isset($_POST['search'])) {
+    $pagination = paginate_page("SELECT COUNT(`order_id`) FROM `orders` WHERE orders.order_id = '{$_POST['order_id']}'", 10);
+    extract($pagination);
+    $orders = select_all_records("SELECT orders.order_id,
+                                        product.product_id,
+                                        product.product_name,
+                                        quantity,
+                                        user_id,
+                                        total_amount,
+                                        placed_on,
+                                        order_status.status_name FROM orders 
+                                        INNER JOIN order_detail ON orders.order_id = order_detail.order_id
+                                        INNER JOIN order_status ON orders.status_id = order_status.status_id 
+                                        INNER JOIN product ON product.product_id = order_detail.product_id 
+                                        WHERE orders.order_id = {$_POST['order_id']}
+                                        GROUP BY orders.placed_on
+                                        ORDER BY MONTH(placed_on) DESC, DAY(placed_on) DESC
+                                        LIMIT {$startIndex}, {$qty}");
+}
+
 ?>
 <style>
     td {
@@ -43,22 +69,27 @@ if (isset($_GET['filterby']) && is_numeric($_GET['filterby'])) {
 
 <div class="container py-5">
     <h1 class="text-center mt-3 mb-5">Order List</h1>
-    <div class="dropdown mb-3">
-        <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
-            Filter By
-        </a>
-        <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-            <li><a class="dropdown-item" href=<?php echo "?page=order-list" ?>>All</a></li>
-            <?php
-            foreach ($orderStatus as $stt) :
-                extract($stt);
-            ?>
-                <li><a class="dropdown-item" href=<?php echo "?page=order-list&tabindex=1&filterby={$status_id}" ?>><?= ucfirst($status_name) ?></a></li>
-            <?php
-            endforeach;
-            ?>
-        </ul>
+    <div class="d-flex justify-content-between align-items-center mb-5">
+        <!-- filter order -->
+        <div class="">
+            <select class="form-select rounded-pill" name="" onchange="window.location = this.value">
+                <option value="?page=order-list">All</option>
+                <?php foreach ($orderStatus as $stt) :  extract($stt) ?>
+                    <option value=<?php echo "?page=order-list&filterby={$status_id}" ?> <?php echo isset($_GET['filterby']) && $status_id == $_GET['filterby'] ? "selected" : "" ?>><?= ucfirst($status_name)  ?></option>
+                <?php endforeach; ?>
+            </select>
+        </div>
+        <!-- search order -->
+        <div class="">
+            <form action="" method="POST">
+                <div class="input-group rounded-pill d-flex align-items-center gap-2 px-2 border border-1 border-dark">
+                    <input type="number" name="order_id" class="form-control rounded-pill border-0 bg-transparent" id="search-input" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
+                    <button type="submit" name="search" class="border-0" style="background-color: transparent"><i class="fas fa-search"></i></button>
+                </div>
+            </form>
+        </div>
     </div>
+
     <form action="?page=order-update" method="POST" onsubmit="return check()">
         <table class="table">
             <thead>
