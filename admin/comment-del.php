029a940170700a00707e4e4cbf7415f5c7e399e8
PeoScouser98 <quanghiep03198@gmail.com>
2022-06-16 17:46:39 +0700

    fix lỗi xóa sản phẩm

--- admin/comment-del.php
+++ admin/comment-del.php
@@ -3,13 +3,17 @@ if (isset($_GET['delId'])) {
     $delid   =  $_GET['delId'];
     $sql = "DELETE FROM comments WHERE comment_id = '{$delid}'";
     execute_query($sql);
-    echo "<script>history.go(-1)</script>";
+    echo "<script>window.location = '?page=comment-list'</script>";
 }
 if (isset($_POST['delete-all']) && isset($_POST['delId'])) {
     if (is_array($_POST['delId'])) {
         foreach ($_POST['delId'] as $id) :
             execute_query("DELETE FROM comments WHERE comment_id = '{$id}'");
         endforeach;
-        echo "<script>history.go(-1)</script>";
+        echo "<script>window.location = '?page=comment-list'</script>";
     }
+}
+if (!isset($_GET['delId'])) {
+    echo "<script>alert(`No item is chosen!`)</script>";
+    echo "<script>window.location = '?page=comment-list'</script>";
 }
