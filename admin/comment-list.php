3718888b1935b05786d218306a759238e69353ea
PeoScouser98 <quanghiep03198@gmail.com>
2022-06-16 15:37:42 +0700

    thêm tính năng lọc bình luận
    lọc theo bình luận mới nhất và cũ nhất

--- admin/comment-list.php
+++ admin/comment-list.php
@@ -1,11 +1,19 @@
 <?php
+$order = isset($_GET['orderby']) ? $_GET['orderby'] : "DESC";
 $sql = "SELECT * FROM comments 
         INNER JOIN users ON comments.user_id = users.user_id
-        INNER JOIN product ON comments.product_id = product.product_id";
+        INNER JOIN product ON comments.product_id = product.product_id
+        ORDER BY YEAR(comment_date) {$order}, MONTH(comment_date) {$order}, DAY(comment_date) {$order}";
 $comments = select_all_records($sql);
 ?>
 <div class="container bg-white py-5">
     <h1 class="text-center mb-5">Comment List</h1>
+    <div class="mb-5">
+        <select class="form-select rounded-pill w-auto" name="" id="">
+            <option value=<?php echo "/admin?page=comment-list&orderby=asc" ?> <?php echo isset($_GET['orderby']) && $_GET['orderby'] == "desc" ? "selected" : "" ?>>Newest comments</option>
+            <option value=<?php echo "/admin?page=comment-list&orderby=desc" ?> <?php echo isset($_GET['orderby']) && $_GET['orderby'] == "asc" ? "selected" : "" ?>>Lasted comments</option>
+        </select>
+    </div>
     <form action="?page=comment-del" method="POST" onsubmit="return confirm(`Do you want to delete all of these comments?`)">
         <table class="table">
             <thead>
